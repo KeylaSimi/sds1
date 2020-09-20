2c9c6f25a92cd69f765eba8af2ae86f3397c00f3
Keyla Simi <keylasimi@outlook.com>
2020-09-20 13:15:53 -0300

    Criando página home

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Home from './pages/Home';
 /*import Hello from './Hello';*/
 
 /*function App() {
@@ -18,6 +19,7 @@ function App() {
   return (
   <div>  
     <Header />
+    <Home />
   </div> 
   );
 }
